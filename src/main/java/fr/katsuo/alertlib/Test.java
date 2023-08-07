9e2291bbab0a680230fbeb8a756d190eb3daf289
Your Name <you@example.com>
2023-08-07 17:38:39 +0200

    init 2

--- src/main/java/fr/katsuo/alertlib/Test.java
+++ src/main/java/fr/katsuo/alertlib/Test.java
@@ -1,7 +1,10 @@
 package fr.katsuo.alertlib;
 
+import fr.katsuo.alertlib.utils.AlertEnum;
+
 public class Test {
     public static void main(String[] args) {
-
+        AlertLib alertLib = new AlertLib("Minecraft Launcher");
+        alertLib.sendAlert(AlertEnum.ERROR, "");
     }
 }
---
+++ src/main/java/fr/katsuo/alertlib/utils/AlertEnum.java
@@ -0,0 +1,5 @@
+package fr.katsuo.alertlib.utils;
+
+public enum AlertEnum {
+    SUCCESS, INFO, WARNING, ERROR
+}
